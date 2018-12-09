c3f1ee3cd581c98d87705c95afd7b80c60f08035
uzum <a.uzumcuoglu@gmail.com>
2018-12-09 13:21:57 +0000

    draw migration count graphs in the timeline

--- tools/plotters/timeline.py
+++ tools/plotters/timeline.py
@@ -3,9 +3,9 @@ import numpy as np
 import csv
 
 markers = {
-    'heuristic': { 'timestamps': [], 'repl': [], 'repl_err': [], 'cost': [], 'cost_err': [], 'drop': [], 'drop_err': [], 'effc': [], 'effc_err': [] },
-    'optimal': { 'timestamps': [], 'repl': [], 'repl_err': [], 'cost': [], 'cost_err': [], 'drop': [], 'drop_err': [], 'effc': [], 'effc_err': [] },
-    'normal': { 'timestamps': [], 'repl': [], 'repl_err': [], 'cost': [], 'cost_err': [], 'drop': [], 'drop_err': [], 'effc': [], 'effc_err': [] }
+    'heuristic': { 'timestamps': [], 'repl': [], 'repl_err': [], 'cost': [], 'cost_err': [], 'migr': [], 'migr_err': [], 'drop': [], 'drop_err': [], 'effc': [], 'effc_err': [] },
+    'optimal': { 'timestamps': [], 'repl': [], 'repl_err': [], 'cost': [], 'cost_err': [], 'migr': [], 'migr_err': [], 'drop': [], 'drop_err': [], 'effc': [], 'effc_err': [] },
+    'normal': { 'timestamps': [], 'repl': [], 'repl_err': [], 'cost': [], 'cost_err': [], 'migr': [], 'migr_err': [], 'drop': [], 'drop_err': [], 'effc': [], 'effc_err': [] }
 }
 
 color_map = {
@@ -21,19 +21,22 @@ for algorithm in ['optimal', 'heuristic', 'normal']:
             markers[algorithm]['timestamps'].append(float(row[1].replace(',', '')))
             markers[algorithm]['repl'].append(float(row[2].replace(',', '')))
             markers[algorithm]['cost'].append(float(row[3].replace(',', '')))
-            markers[algorithm]['drop'].append(float(row[6].replace(',', '')))
-            markers[algorithm]['effc'].append(float(row[7].replace(',', '')))
-            markers[algorithm]['repl_err'].append(float(row[8].replace(',', '')) - float(row[2].replace(',', '')))
-            markers[algorithm]['cost_err'].append(float(row[10].replace(',', '')) - float(row[3].replace(',', '')))
-            markers[algorithm]['drop_err'].append(float(row[12].replace(',', '')) - float(row[6].replace(',', '')))
-            markers[algorithm]['effc_err'].append(float(row[14].replace(',', '')) - float(row[7].replace(',', '')))
+            markers[algorithm]['migr'].append(float(row[4].replace(',', '')))
+            markers[algorithm]['drop'].append(float(row[7].replace(',', '')))
+            markers[algorithm]['effc'].append(float(row[8].replace(',', '')))
+            markers[algorithm]['repl_err'].append(float(row[9].replace(',', '')) - float(row[2].replace(',', '')))
+            markers[algorithm]['cost_err'].append(float(row[11].replace(',', '')) - float(row[3].replace(',', '')))
+            markers[algorithm]['migr_err'].append(float(row[13].replace(',', '')) - float(row[4].replace(',', '')))
+            markers[algorithm]['drop_err'].append(float(row[15].replace(',', '')) - float(row[7].replace(',', '')))
+            markers[algorithm]['effc_err'].append(float(row[17].replace(',', '')) - float(row[8].replace(',', '')))
 
-fig, axs = plt.subplots(nrows=4, ncols=1, figsize=(12, 24))
+fig, axs = plt.subplots(nrows=5, ncols=1, figsize=(12, 30))
 for algorithm in ['optimal', 'heuristic', 'normal']:
-    axs[0].errorbar(markers[algorithm]['timestamps'], markers[algorithm]['repl'], yerr=markers[algorithm]['repl_err'], fmt='o', capsize=3, color = color_map[algorithm], alpha=0.5, label=algorithm)
-    axs[1].errorbar(markers[algorithm]['timestamps'], markers[algorithm]['cost'], yerr=markers[algorithm]['cost_err'], fmt='o', capsize=3, color = color_map[algorithm], alpha=0.5, label=algorithm)
-    axs[2].errorbar(markers[algorithm]['timestamps'], markers[algorithm]['drop'], yerr=markers[algorithm]['drop_err'], fmt='o', capsize=3, color = color_map[algorithm], alpha=0.5, label=algorithm)
-    axs[3].errorbar(markers[algorithm]['timestamps'], markers[algorithm]['effc'], yerr=markers[algorithm]['effc_err'], fmt='o', capsize=3, color = color_map[algorithm], alpha=0.5, label=algorithm)
+    axs[0].errorbar(markers[algorithm]['timestamps'], markers[algorithm]['repl'], yerr=markers[algorithm]['repl_err'], fmt='o', capsize=3, color = color_map[algorithm], alpha=0.7, label=algorithm)
+    axs[1].errorbar(markers[algorithm]['timestamps'], markers[algorithm]['cost'], yerr=markers[algorithm]['cost_err'], fmt='o', capsize=3, color = color_map[algorithm], alpha=0.7, label=algorithm)
+    axs[2].errorbar(markers[algorithm]['timestamps'], markers[algorithm]['drop'], yerr=markers[algorithm]['drop_err'], fmt='o', capsize=3, color = color_map[algorithm], alpha=0.7, label=algorithm)
+    axs[3].errorbar(markers[algorithm]['timestamps'], markers[algorithm]['effc'], yerr=markers[algorithm]['effc_err'], fmt='o', capsize=3, color = color_map[algorithm], alpha=0.7, label=algorithm)
+    axs[4].errorbar(markers[algorithm]['timestamps'], markers[algorithm]['migr'], yerr=markers[algorithm]['migr_err'], fmt='o', capsize=3, color = color_map[algorithm], alpha=0.7, label=algorithm)
 
 axs[0].set_xlabel('Time')
 axs[0].set_ylabel('Replication factor')
@@ -47,6 +50,9 @@ axs[2].legend()
 axs[3].set_xlabel('Time')
 axs[3].set_ylabel('Energy efficiency')
 axs[3].legend()
+axs[4].set_xlabel('Time')
+axs[4].set_ylabel('Migration count')
+axs[4].legend()
 
 
 plt.tight_layout()
